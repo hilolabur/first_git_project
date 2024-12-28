231996ccdabe5c94c20c0d00315ce4b8e8cfa58d
hilolabur <khilola.b5@gmail.com>
2024-12-28 13:51:46 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello ydeo");
+        System.out.println("Hello Programmers");
+
+        
+
+        
     }
 }
